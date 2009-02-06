ba426122d103e018be4708242159a404d0134ec5
Gael Pasgrimaud <gael@gawel.org>
2009-02-06 04:18:02 +0100

    new script

--- extras/firefox/apwalfr/build.sh
+++ extras/firefox/apwalfr/build.sh
@@ -30,99 +30,67 @@
 #
 # default configuration file is ./config_build.sh, unless another file is 
 # specified in command-line. Available config variables:
-APP_NAME=          # short-name, jar and xpi files name. Must be lowercase with no spaces
-CHROME_PROVIDERS=  # which chrome providers we have (space-separated list)
-CLEAN_UP=          # delete the jar / "files" when done?       (1/0)
-ROOT_FILES=        # put these files in root of xpi (space separated list of leaf filenames)
-ROOT_DIRS=         # ...and these directories       (space separated list)
-BEFORE_BUILD=      # run this before building       (bash command)
-AFTER_BUILD=       # ...and this after the build    (bash command)
+#!/bin/bash
+# build.sh: build JAR and XPI files from source
+# based on Nathan Yergler's build script
+# Modified to work on OS X by Mike Chambers (http://mesh.typepad.com)
 
-if [ -z $1 ]; then
-  . ./config_build.sh
-else
-  . $1
-fi
+#### editable items (none of these can be blank)
+APP_NAME=apwalfr     # short-name, jar and xpi files name.
+HAS_DEFAULTS=0       # whether the ext. provides default values for user prefs etc.
+HAS_COMPONENTS=0     # whether the ext. includes any components
+HAS_LOCALE=1         # package APP_NAME.jar/locale/ ?
+HAS_SKIN=1           # package APP_NAME.jar/skin/ ?
+KEEP_JAR=1           # leave the jar when done?
+ROOT_FILES="license.txt install.rdf install.js" # put these files in root of xpi
+#### editable items end
 
-if [ -z $APP_NAME ]; then
-  echo "You need to create build config file first!"
-  echo "Read comments at the beginning of this script for more info."
-  exit;
-fi
-
-ROOT_DIR=`pwd`
-TMP_DIR=build
+TMP_DIR=build.tmp
 
 #uncomment to debug
-#set -x
+set -x
 
-# remove any left-over files from previous build
-rm -f $APP_NAME.jar $APP_NAME.xpi files
+# remove any left-over files
+rm $APP_NAME.jar
+rm $APP_NAME.xpi
 rm -rf $TMP_DIR
 
-$BEFORE_BUILD
+# create xpi directory layout and populate it
+mkdir $TMP_DIR
+mkdir $TMP_DIR/chrome
 
-mkdir --parents --verbose $TMP_DIR/chrome
+if [ $HAS_COMPONENTS = 1 ]; then
+  mkdir $TMP_DIR/components
+  cp components/* $TMP_DIR/components
+fi
 
-# generate the JAR file, excluding CVS and temporary files
-JAR_FILE=$TMP_DIR/chrome/$APP_NAME.jar
-echo "Generating $JAR_FILE..."
-for CHROME_SUBDIR in $CHROME_PROVIDERS; do
-  find $CHROME_SUBDIR -path '*CVS*' -prune -o -type f -print | grep -v \~ >> files
-done
-
-zip -0 -r $JAR_FILE `cat files`
-# The following statement should be used instead if you don't wish to use the JAR file
-#cp --verbose --parents `cat files` $TMP_DIR/chrome
-
-# prepare components and defaults
-echo "Copying various files to $TMP_DIR folder..."
-for DIR in $ROOT_DIRS; do
-  mkdir $TMP_DIR/$DIR
-  FILES="`find $DIR -path '*CVS*' -prune -o -type f -print | grep -v \~`"
-  echo $FILES >> files
-  cp --verbose --parents $FILES $TMP_DIR
-done
+if [ $HAS_DEFAULTS = 1 ]; then
+  DEFAULT_FILES="`find ./defaults -path '*DS_Store*' -prune -o -type f -print | grep -v \~`"
+  cp --parents $DEFAULT_FILES $TMP_DIR
+fi
 
 # Copy other files to the root of future XPI.
-for ROOT_FILE in $ROOT_FILES install.rdf chrome.manifest; do
-  cp --verbose $ROOT_FILE $TMP_DIR
-  if [ -f $ROOT_FILE ]; then
-    echo $ROOT_FILE >> files
-  fi
-done
+cp $ROOT_FILES $TMP_DIR
 
-cd $TMP_DIR
-
-if [ -f "chrome.manifest" ]; then
-  echo "Preprocessing chrome.manifest..."
-  # You think this is scary?
-  #s/^(content\s+\S*\s+)(\S*\/)$/\1jar:chrome\/$APP_NAME\.jar!\/\2/
-  #s/^(skin|locale)(\s+\S*\s+\S*\s+)(.*\/)$/\1\2jar:chrome\/$APP_NAME\.jar!\/\3/
-  #
-  # Then try this! (Same, but with characters escaped for bash :)
-  sed -i -r s/^\(content\\s+\\S*\\s+\)\(\\S*\\/\)$/\\1jar:chrome\\/$APP_NAME\\.jar!\\/\\2/ chrome.manifest
-  sed -i -r s/^\(skin\|locale\)\(\\s+\\S*\\s+\\S*\\s+\)\(.*\\/\)$/\\1\\2jar:chrome\\/$APP_NAME\\.jar!\\/\\3/ chrome.manifest
-
-  # (it simply adds jar:chrome/whatever.jar!/ at appropriate positions of chrome.manifest)
+# generate the JAR file, excluding .DS_Store and temporary files
+zip -0 -r $TMP_DIR/chrome/$APP_NAME.jar `find content -path '*DS_Store*' -prune -o -type f -print | grep -v \~`
+if [ $HAS_LOCALE = 1 ]; then
+  zip -0 -r $TMP_DIR/chrome/$APP_NAME.jar `find locale -path '*DS_Store*' -prune -o -type f -print | grep -v \~`
+fi
+if [ $HAS_SKIN = 1 ]; then
+  zip -0 -r $TMP_DIR/chrome/$APP_NAME.jar `find skin -path '*DS_Store*' -prune -o -type f -print | grep -v \~`
 fi
 
 # generate the XPI file
-echo "Generating $APP_NAME.xpi..."
+cd $TMP_DIR
 zip -r ../$APP_NAME.xpi *
+cd ..
 
-cd "$ROOT_DIR"
-
-echo "Cleanup..."
-if [ $CLEAN_UP = 0 ]; then
+if [ $KEEP_JAR = 1 ]; then
   # save the jar file
   mv $TMP_DIR/chrome/$APP_NAME.jar .
-else
-  rm ./files
 fi
 
 # remove the working files
 rm -rf $TMP_DIR
-echo "Done!"
 
-$AFTER_BUILD
