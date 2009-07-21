47b44c1a9189949db10e033d98ec2c28000d2cc4
Gael Pasgrimaud <gael@gawel.org>
2009-07-21 19:43:23 +0200

    clean api

--- extras/firefox/apwalfr/content/overlay.js
+++ extras/firefox/apwalfr/content/overlay.js
@@ -1,64 +1,32 @@
-/* ***** BEGIN LICENSE BLOCK *****
- *   Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- * 
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is a.pwal.fr.
- *
- * The Initial Developer of the Original Code is
- * Gael Pasgrimaud.
- * Portions created by the Initial Developer are Copyright (C) 2009
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- * 
- * ***** END LICENSE BLOCK ***** */
-
 var apwalfr = {
   _doc: null,
+  serv: 'http://a.pwal.fr',
+
   onLoad: function() {
     // initialization code
     this.initialized = true;
   },
+
+  addScript: function(url) {
+    var doc = apwalfr._doc;
+    var s = doc.createElement('script');
+    s.setAttribute('src', apwalfr.serv+url);
+    s.setAttribute('type', 'text/javascript');
+    doc.body.appendChild(s);
+  },    
 
   onMenuItemCommand: function(event) {
     // load api
     var doc = window.content.document;
     apwalfr._doc = doc;
     if (jQuery('script#apwal-api').length == 0) {
-            var s = doc.createElement('script');
-            s.setAttribute('id', 'apwal-api');
-            s.setAttribute('src', 'http://a.pwal.fr/_static/api.js');
-            s.setAttribute('type', 'text/javascript');
-            doc.body.appendChild(s);
+        apwalfr.addScript('/_static/api.js');
     }
 
     if (event.button != 0)
         return;
                          
     var url = doc.location;
-    // var src = 'http://localhost:5000';
-    var src = 'http://a.pwal.fr';
 
     jQuery('#apwalfr', doc).remove();
     jQuery('#apwalfr_close', doc).remove();
@@ -67,32 +35,25 @@ var apwalfr = {
     jQuery('body', doc).prepend('' +
         '<div id="apwalfr_close" onclick="'+close+'" ' +
              'style=z-index:1000001;float:left;top:55px;left:435px;position:fixed;border:0px;display:block;">' +
-          '<img style="width:20px;height:20px;border:0px;" src="'+src+'/_static/images/close.png" />' +
+          '<img style="width:20px;height:20px;border:0px;" src="'+apwalfr.serv+'/_static/images/close.png" />' +
         '</div>' +
-        '<iframe src="'+src+'/?p=firefox&post='+url+'" id="apwalfr"' +
+        '<iframe src="'+apwalfr.serv+'/?p=firefox&post='+url+'" id="apwalfr"' +
                 'style="z-index:1000000;float:left;top:50px;left:30px;position:fixed;border:thin solid black; width:430px; height:230px;"' +
                ' />' +
         '</iframe>'+
         '');
   },
   onQuick: function(obj, type) {
-    var doc = apwalfr._doc;
-    var s = doc.createElement('script');
-    s.setAttribute('src', 'http://a.pwal.fr/json/?callback=apwal.'+type+'Quick&url='+doc.location);
-    s.setAttribute('type', 'text/javascript');
-    doc.body.appendChild(s);
-  }
+    apwalfr.addScript('/json/?callback=apwal.'+type+'Quick&url='+apwalfr._doc.location);
+  },
   onShowStats: function(obj) {
     var doc = apwalfr._doc;
-    jQuery('a[href^="http://a.pwal.fr/"]', doc).each(function() {
+    jQuery('a.apwalfr_link', doc).remove();
+    jQuery('a[href^="'+apwalfr.serv+'/"]', doc).each(function() {
             var link = jQuery(this);
             var id = link.attr('href').split('/')[3];
-            jQuery('a#apwalfr_'+id).remove();
-            link.after(' <a id="apwalfr_'+id+'" href="http://a.pwal.fr/stats/'+id+'" target="_blank"></a>');
-            var s = doc.createElement('script');
-            s.setAttribute('src', 'http://a.pwal.fr/json/stats/?callback=apwal.showStats&alias='+id);
-            s.setAttribute('type', 'text/javascript');
-            doc.body.appendChild(s);
+            link.after(' <a id="apwalfr_'+id+'" class="apwalfr_link" href="'+apwalfr.serv+'/stats/'+id+'" target="_blank"></a>');
+            apwalfr.addScript('/json/stats/?callback=apwal.showStats&alias='+id);
     });
   }
 
